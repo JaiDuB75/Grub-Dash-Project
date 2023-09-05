42ffbfa90fffb65970a859b2d4cb420abb3fc0b2
Jairus Ezra DuBose <dubosejairus@gmail.com>
2023-09-05 00:01:20 -0400

    Dishes: List, and Create Function working, Read not working

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,44 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+function list(req, res, next){
+    res.json({data: dishes});//
+}
+
+function create(req, res){
+    //const reqBody = res.locals.reqBody; 
+    const {data: {name, description, price, image_url} = {} } = req.body; 
+
+    
+
+    const newDish = {
+        id: nextId(),
+        name,
+        description,
+        price,
+        image_url,
+
+    };
+
+    //console.log(newDish);
+    dishes.push(newDish);
+    res.status(201).json({data: newDish});
+}
+
+function read(req, res){
+    const dish = res.locals.dish;
+    console.log(dish);
+    res.json({data: dish});
+}
+
+//export functions
+module.exports = {
+    create:[
+        create,
+    ],
+    read: [
+        read,
+    ],
+    list,
+}
